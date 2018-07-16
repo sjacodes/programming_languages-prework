947fad2ff4b9b9771999051ffc993ff67d04fd62
Sarah Jacob <smjacob91@gmail.com>
2018-07-16 16:28:54 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,6 +1,18 @@
-def reformat_languages(languages)
-  # your code here
+instructors = [{name: 'Brooke', hometown: 'New York', mood: 'excited'}, {name: 'Daniel', hometown: 'Poland', mood: 'very excited'}, {name: 'Niky', hometown: 'San Francisco', mood: 'super excited'}]
+
+def get_names(instructors)
+  # Define a method called get_names that takes an array of instructors and returns just their names
+  return instructors.map do |instructor|
+    # instructor = {name: 'Brooke', hometown: 'New York', mood: 'excited'}
+    instructor.fetch(:name)
+  end
+  # instructors.map {|instructor| instructor.[:name]}
 end
 
+get_names(instructors)
 
-
+def get_people_from_a_place(people, place)
+  # DON'T USE EACH
+  # use select
+  # return a list of people from place
+end
