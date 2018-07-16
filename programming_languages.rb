0a0175f7bb72f83cf71053149a6b3972d61c2433
Sarah Jacob <smjacob91@gmail.com>
2018-07-16 16:31:18 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,7 +1,6 @@
 instructors = [{name: 'Brooke', hometown: 'New York', mood: 'excited'}, {name: 'Daniel', hometown: 'Poland', mood: 'very excited'}, {name: 'Niky', hometown: 'San Francisco', mood: 'super excited'}]
 
 def get_names(instructors)
-  # Define a method called get_names that takes an array of instructors and returns just their names
   return instructors.map do |instructor|
     # instructor = {name: 'Brooke', hometown: 'New York', mood: 'excited'}
     instructor.fetch(:name)
@@ -15,4 +14,5 @@ def get_people_from_a_place(people, place)
   # DON'T USE EACH
   # use select
   # return a list of people from place
+  
 end
